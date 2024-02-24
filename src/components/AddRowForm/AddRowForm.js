f653eb869aca36879c6e2107d2acbaab94acbc20
suyashtiwari0902 <suyashtiwari0209@gmail.com>
2024-02-24 14:02:36 +0530

    [Styling] Styled the App using MaterialUI

--- src/components/AddRowForm/AddRowForm.js
+++ src/components/AddRowForm/AddRowForm.js
@@ -1,7 +1,8 @@
-
 import React, { useState } from 'react';
-import './AddRowForm.css';
+import { useTheme } from '@emotion/react';
+
 const AddRowForm = ({ onAddRow }) => {
+  const theme = useTheme();
   const initialFormData = {
     ID: '',
     'Avatar Name': '',
@@ -23,31 +24,78 @@ const AddRowForm = ({ onAddRow }) => {
     onAddRow(formData);
     setFormData(initialFormData); 
   };
+  const formStyle = {
+    maxWidth: '400px',
+    margin: '20px auto',
+    padding: '20px',
+    backgroundColor: theme.palette.background.default,
+    boxShadow: theme.shadows[3],
+    borderRadius: '8px',
+  };
+  const inputStyle = {
+    width: '100%',
+    padding: '8px',
+    fontSize: '16px',
+    border: `1px solid ${theme.palette.text.primary}`,
+    borderRadius: '4px',
+    boxSizing: 'border-box',
+    marginBottom: '10px',
+  };
+  
+  const buttonStyle = {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.text.primary,
+    padding: '10px 20px',
+    fontSize: '16px',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s',
+  };
+  
 
   return (
-    <form className="add-row-form" onSubmit={handleSubmit}>
+      <form style={formStyle} onSubmit={handleSubmit}>
         <div className='form-group'>
-      <label htmlFor='ID'> ID: </label>
-        <input type="text" id="ID" name="ID" value={formData.ID} onChange={handleChange} />
+          <label htmlFor='ID'> ID: </label>
+          <input
+            type="text"
+            id="ID"
+            name="ID"
+            value={formData.ID}
+            onChange={handleChange}
+            style={inputStyle}
+          />
         </div>
         <div className='form-group'>
-
+          <label htmlFor='avatarName'>
+            Avatar Name:
+          </label>
+          <input
+            type="text"
+            id="avatarName"
+            name="Avatar Name"
+            value={formData['Avatar Name']}
+            onChange={handleChange}
+            style={inputStyle}
+          />
         </div>
         <div className='form-group'>
-      <label htmlFor='avatarName'>
-        Avatar Name:
-      </label>
-        <input type="text" id="avatarName" name="Avatar Name" value={formData['Avatar Name']} onChange={handleChange} />
+          <label htmlFor='performanceScore'>
+            Performance Score:
+          </label>
+          <input
+            type="text"
+            id="performanceScore"
+            name="Performance Score"
+            value={formData['Performance Score']}
+            onChange={handleChange}
+            style={inputStyle} 
+          />
         </div>
-        <div className='form-group'>
-      <label htmlFor='performanceScore'>
-        Performance Score:
-      </label>
-        <input type="text" id="performanceScore" name="Performance Score" value={formData['Performance Score']} onChange={handleChange} />
-      </div>
-      <button type="submit">Add Row</button>
-    </form>
-  );
+        <button type="submit" style={buttonStyle}>Add Row</button> 
+      </form>
+    );
 };
 
 export default AddRowForm;
--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,11 +1,66 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  ButtonGroup,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  AppBar,
+  Toolbar,
+  Typography,
+  Switch,
+  Container,
+  CssBaseline,
+  FormControlLabel,
+  createTheme,
+  ThemeProvider,
+ 
+} from '@mui/material';
 import './Homepage.css'; 
 import AddRowForm from "../AddRowForm/AddRowForm";
+
+const themes = {
+  dark: createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#2196f3', 
+      },
+      secondary: {
+        main: '#4caf50', 
+      },
+    },
+    typography: {
+          fontFamily: 'monospace, sans-serif',
+          fontWeight: 'bold' 
+        },
+  }),
+  light: createTheme({
+    palette: {
+      mode: 'light',
+      primary: {
+        main: '#3498db',
+      },
+      secondary: {
+        main: '#2ecc71',
+      },
+    },
+    typography: {
+          fontFamily: 'monospace, sans-serif',
+          fontWeight: 'bold' 
+        },
+  }),
+};
 const Homepage = () => {
     const [data, setData] = useState([]);
     const [showTable, setShowTable] = useState(false);
     const [showForm, setShowForm] = useState(false);
     const [autoSync, setAutoSync] = useState(false);
+    const [darkMode, setDarkMode] = useState(false);
 
     const fetchDataFromSheet = async () => {
       try {
@@ -56,45 +111,95 @@ const Homepage = () => {
       }, [autoSync]);
 
     const columns = ["ID", "Avatar Name", "Performance Score"];
-  
+
+    const toggleDarkMode = () => {
+      setDarkMode(!darkMode);
+    };
+
     return (
+      <ThemeProvider theme={darkMode ? themes.dark : themes.light}>
+      <CssBaseline />
       <div className="Homepage">
-        <header className="app-header">
-        <h1>SheetsApp</h1>
-        </header>
-        <div className="buttons-container">
-        <button className="sync-button" onClick={fetchDataFromSheet}>
-          Sync Data
-        </button>
-        <button className="add-record-button" onClick={toggleForm}>
-          Add Record
-        </button>
-        <button className={`auto-sync-button ${autoSync ? 'active' : ''}`} onClick={toggleAutoSync}>
-          Auto Sync is {autoSync ? 'ON' : 'OFF'}
-        </button>
+        <AppBar position="sticky">
+          <Toolbar>
+            <Typography variant="h3" fontFamily={"monospace"} fontWeight='bold'>SHEETSAPP</Typography>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="lg">
+          <div className="button-group">
+            <FormControlLabel
+            control={
+
+            <Switch
+              className={`auto-sync-button ${autoSync ? 'active' : ''}`}
+              onChange={toggleAutoSync}
+              color="primary"
+              size="large"
+            />
+            }
+            label = {
+            <div className="switch-label">
+            <Typography className="switch-label-text" variant="body1">
+              Auto Sync is {autoSync ? 'ON' : 'OFF'}
+            </Typography>
+            </div>
+            }
+          
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  className={`auto-sync-button ${darkMode ? 'active' : ''}`}
+                  onChange={toggleDarkMode}
+                  color="primary"
+                  size="large"
+                />
+              }
+              label={
+                <div className="switch-label">
+                  <Typography className="switch-label-text" variant="body1">
+                    {darkMode ? 'Dark Mode' : 'Light Mode'}
+                  </Typography>
+                </div>
+              }
+            />
+            <ButtonGroup variant="contained">
+            <Button className="sync-button" onClick={fetchDataFromSheet}>
+              Sync Data
+            </Button>
+            <Button className="add-record-button" onClick={toggleForm}>
+              Add Record
+            </Button>
+            </ButtonGroup>
+          </div>
+          {showForm && 
+            <AddRowForm onAddRow={addRow} />
+}
+          {showTable && (
+            <TableContainer component={Paper} elevation={10} className="data-table-container">
+              <Table className="data-table" aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    {columns.map((columnName, index) => (
+                      <TableCell key={index}>{columnName}</TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {data.map((row, rowIndex) => (
+                    <TableRow key={rowIndex}>
+                      {columns.map((columnName, columnIndex) => (
+                        <TableCell key={columnIndex}>{row[columnName]}</TableCell>
+                      ))}
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
+        </Container>
       </div>
-        {showForm && <AddRowForm onAddRow={addRow}/> }
-        {showTable && (
-            <table className="data-table">
-            <thead>
-            <tr>
-            {columns.map((columnName, index) => (
-              <th key={index}>{columnName}</th>
-            ))}
-            </tr>
-          </thead>
-      <tbody>
-        {data.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {columns.map((columnName, columnIndex) => (
-            <td key={columnIndex}>{row[columnName]}</td>
-          ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-        )}
-      </div>
+    </ThemeProvider>
     );
   };
   
